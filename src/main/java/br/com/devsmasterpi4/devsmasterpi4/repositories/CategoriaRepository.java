f8182502ca712c2022cdf3ee04cf19209b6ac9fc
NailsonNascimento-Dev <nailson_bingo@hotmail.com>
2021-03-06 21:34:54 -0300

    Classe Categoria Finalizada 1.0

---
+++ src/main/java/br/com/devsmasterpi4/devsmasterpi4/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package br.com.devsmasterpi4.devsmasterpi4.repositories;
+
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.devsmasterpi4.devsmasterpi4.dominio.Categoria;
+
+@Repository
+	public interface CategoriaRepository extends PagingAndSortingRepository<Categoria, Integer> {
+	
+
+}
---
+++ src/main/java/br/com/devsmasterpi4/devsmasterpi4/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package br.com.devsmasterpi4.devsmasterpi4.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+
+
+import br.com.devsmasterpi4.devsmasterpi4.dominio.Categoria;
+import br.com.devsmasterpi4.devsmasterpi4.repositories.CategoriaRepository;
+
+@Service
+@Qualifier
+public class CategoriaService {
+	
+
+}
